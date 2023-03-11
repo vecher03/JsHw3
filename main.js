825cc2ab7a006654205eaef682ef6cef0d00bfc6
Ogohi <phoenix.cod4@mail.ru>
2023-03-11 13:54:20 +0500

    save

---
+++ main.js
@@ -0,0 +1,79 @@
+
+
+var time = +prompt ('Введите время')
+
+switch(time) {
+    case 0:
+    alert('Двеннадцать часов ночи')
+    break
+    case 1:
+    alert('Час ночи')
+    break
+    case 2:
+    alert('Два часа ночи')
+    break
+    case 3:
+    alert('Три часа ночи')
+    break
+    case 4:
+    alert('Четыре часа ночи')
+    break
+    case 5:
+    alert('Пять утра')
+    break
+    case 6:
+    alert('Шесть утра')
+    break
+    case 7:
+    alert('Семь утра')
+    break
+    case 8:
+    alert('Восемь утра')
+    break
+    case 9:
+    alert('Девять часов утра')
+    break
+    case 10:
+    alert('Десять часов утра')
+    break
+    case 11:
+    alert('Одиннадцать часов утра')
+    break
+    case 12:
+    alert('Двенадцать часов дня')
+    break
+    case 13:
+    alert('Час дня')
+    break
+    case 14:
+    alert('Два часа дня')
+    break
+    case 15:
+    alert('Три часа дня')
+    break
+    case 16:
+    alert('Четыре часа дня')
+    break
+    case 17:
+    alert('Пять часов вечера')
+    break
+    case 18:
+    alert('Шесть часов вечера')
+    break
+    case 19:
+    alert('Семь часов вечера')
+    break
+    case 20:
+    alert('Восемь часов вечера')
+    break
+    case 21:
+    alert('Девять часов вечера')
+    break
+    case 22:
+    alert('Десять часов вечера')
+    break
+    case 23:
+    alert('Одинадцать часов вечера')
+    break
+
+}
